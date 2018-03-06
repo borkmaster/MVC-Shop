d93add30294a45935d1f69fec04aba39e19bab91
borkmaster <unluckymattress@gmail.com>
2018-03-06 02:30:41 -0600

    Add Generic In-Memory Repository

---
+++ MVC-Shop/MVC-Shop.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,84 @@
+﻿using MVC_Shop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Shop.DataAccess.InMemory
+{
+    public class InMemoryRepository<T> where T : BaseEntity
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string className;
+
+        public InMemoryRepository()
+        {
+            className = typeof(T).Name;
+            items = cache[className] as List<T>;
+            if (items == null)
+            {
+                items = new List<T>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache[className] = items;
+        }
+
+        public void Insert(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Update(T item)
+        {
+            T itemToUpdate = items.Find(i => i.Id == item.Id);
+
+            if ( itemToUpdate != null )
+            {
+                itemToUpdate = item;
+            }
+            else
+            {
+                throw new Exception(className + " not found");
+            }
+        }
+
+        public T Find(string Id)
+        {
+            T item = items.Find(i => i.Id == Id);
+
+            if ( item != null )
+            {
+                return item;
+            }
+            else
+            {
+                throw new Exception(className + " not found");
+            }
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            T itemToRemove = items.Find(i => i.Id == Id);
+
+            if (itemToRemove != null)
+            {
+                items.Remove(itemToRemove);
+            }
+            else
+            {
+                throw new Exception(className + " not found");
+            }
+        }
+    }
+}
