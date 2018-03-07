814f1d4234486568c4ae612ea258928728b76e4e
borkmaster <unluckymattress@gmail.com>
2018-03-07 07:48:44 -0600

    Add Basket, BasketItem Model and DB Tables

--- MVC-Shop/MVC-Shop.DataAccess.SQL/DataContext.cs
+++ MVC-Shop/MVC-Shop.DataAccess.SQL/DataContext.cs
@@ -15,5 +15,7 @@ namespace MVC_Shop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
