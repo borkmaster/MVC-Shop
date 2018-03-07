f6d5aca1183d2ef4de74935840250bcc3781cd84
borkmaster <unluckymattress@gmail.com>
2018-03-07 10:48:26 -0600

    Add BasketService interface, BasketItem/BasketSummary view models

---
+++ MVC-Shop/MVC-Shop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using MVC_Shop.Core.ViewModels;
+
+
+namespace MVC_Shop.Services
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+    }
+}
